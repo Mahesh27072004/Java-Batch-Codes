483977a9c47c9130cc2d2926151f1fece4264be5
Mahesh Sanap <144589767+Mahesh27072004@users.noreply.github.com>
2025-02-25 11:56:52 +0530

    commit

---
+++ loops/HighetRepeatingChar.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class HighetRepeatingChar
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter te string ");
+		String str = sc.next().toUpperCase();
+		//System.out.println(str);
+		int max =0 ;
+		char c = 'n';
+		for(char ch='A' ; ch<='Z' ; ch++)
+		{
+			int cnt = 0 ;
+			for(int i = 0 ; i<str.length() ; i++)
+			{
+				if(ch ==  str.charAt(i))
+				{
+					cnt++;
+				}
+			}
+			if(max<cnt)
+			{
+				max=cnt;
+				c = ch;
+			}
+		}
+		System.out.println(" the most repeating char is "+c+" and its freq is "+max);
+	}
+}
